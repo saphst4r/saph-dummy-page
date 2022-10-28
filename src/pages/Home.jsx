e8fc5c670da9f27bcad1e4f5c796d6fd7ad6b9f2
saphst4r <saphst4r@naver.com>
2022-10-28 10:54:38 +0900

    styling changes for font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import logo from '../images/white-logo-1024.png'
 export default function Home() {
   return (
     <div>
-        <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
-            <div className='mt-[200px]'>
-                <img src={logo} alt="saph logo" className='justify-center item-center max-h-[540px]'/>
+        <section className='flex justify-center items-center flex-col fixed left-0 right-0 bottom-0 top-0'>
+            <div className='mt-12 md:mt-20'>
+                <img src={logo} alt="saph logo" className='justify-center item-center md:max-h-[540px] max-h-[280px]'/>
             </div> 
 
-            <p className='text-white font-bold text-6xl mt-12'>SAPH.CO</p>
-            <p className='text-white font-medium text-xl mt-6'>Hello to the bois in OG</p>
+            <h1 className='text-white font-md text-5xl md:text-[90px] mt-6 md:mt-10'>SAPH.CO</h1>
+            <p className='text-white text-md mt-2 md:mt-6'>Hello to the bois in OG</p>
         </section>
 
     </div>
