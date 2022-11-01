b9f7d7da679573bfcb0cedac5cc9ae2cacf5b27a
saphst4r <saphst4r@naver.com>
2022-11-01 21:53:48 +0900

    added a standin works page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Works from "./pages/Works";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/works" element={<Works/>}/>
         </Routes>
       </Router>
     </div>
